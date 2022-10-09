932921b236d4b7803f2acf3482e067e9ff8ffdae
Kamil Grabiec <kamilgrabiec@interia.pl>
2022-10-09 22:22:53 +0200

    longest word re-factor

--- longestWord/app.js
+++ longestWord/app.js
@@ -2,16 +2,32 @@
 
 function longestWord(text) {
     let words = text.split(" ")
-    console.log(words.length)
     let index = 0
     for(let i=0; i< words.length; i++){
-        if(words[0].length < words[i].length) {
+        if(words[index].length < words[i].length) {
             index = i
         }
     }
     return words[index];
 }
 
+function longestWord2(text) {
+    let words = text.split(" ")
+    let longesWord = ""
+
+    for (let word of words) {
+        if(word.length>longesWord.length){
+            longesWord = word
+        }
+    }
+    return longesWord
+}
+
+
+
 
 console.log(longestWord("I woke up early today"));
 console.log(longestWord("I went straight to the beach"));
+
+console.log(longestWord2("I woke up early today"));
+console.log(longestWord2("I went straight to the beach"));
