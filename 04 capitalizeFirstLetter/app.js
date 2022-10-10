e0b1281352b1755bb85074790b498e2ed4ec038c
Kamil Grabiec <kamilgrabiec@interia.pl>
2022-10-10 13:24:29 +0200

    capitalize first letter 2 with map metod

--- 04 capitalizeFirstLetter/app.js
+++ 04 capitalizeFirstLetter/app.js
@@ -2,8 +2,7 @@ let str1 = "I got up early today"
 
 function capitalizeFirstLetter(text){
 
-    let result = ""
-    
+    let result = ""    
     let arr = str1.split(" ")
     for(let word of arr){
         let capitalizeWord = word.charAt(0).toUpperCase() + word.slice(1)
@@ -13,10 +12,17 @@ function capitalizeFirstLetter(text){
     return result.trim()
 }
 
+function capitalizeFirstLetter2(text) {
+    let words = text.split(" ").map(word => {
+        return word.charAt(0).toUpperCase() +  word.slice(1)
+    })
 
+    return words.join(" ")
+}
 
 
 
 
 
 console.log(capitalizeFirstLetter("I got up early today"))
+console.log(capitalizeFirstLetter2("I walked straight to the beach"))
